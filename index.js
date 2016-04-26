baaad014ff636602bed432ecfc3160521398c8a4
DJ Melisso <djm@deltavbio.com>
2016-04-25 19:14:40 -0700

    more modules but still doesn't work

--- index.js
+++ index.js
@@ -4,11 +4,15 @@ var bodyParser = require('body-parser');
 var moment = require('moment');
 var router = express.Router();
 var port = process.env.PORT || 80;
-shortid.seed(6899);
+
+app.set('view engine', 'ejs');
+app.use(express.static('static'));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 router.get('/', function(req, res) {
-  res.render('pages/home');
+  res.render('pages/home.html');
 });
+app.use('/', router);
 
 app.listen(port);
 console.log("Magic happens on port " + port);
